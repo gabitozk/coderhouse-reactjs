4bc9dcebe17ae871c1dbebf489e731ce98871be8
Gabriel Zinyk <gabi.zinyk@gmail.com>
2022-01-06 15:16:36 -0300

    Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import './css/Navbar.css';
+
+export default function Navbar () {
+    return (
+        <header className='header'>
+            <h3>LOGO</h3>
+            <ul className='header__nav'>
+                <li>shop</li>
+                <li>contact</li>
+                <li>about</li>
+                <li>log in</li>
+            </ul>
+        </header>
+    )
+}
