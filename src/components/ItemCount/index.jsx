e38a7bde55baf7df05077b5a45b04437714e98e0
Gabriel Zinyk <gabi.zinyk@gmail.com>
2022-01-19 22:33:38 -0300

    Agregando ItemCount component

---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const ItemCount = ({stock, initial, onAdd}) => {
+    const [cant, setCant] = useState(initial);
+    const [stockk, setStockk] = useState(stock);
+      
+    const sumCant = () => {
+            setCant(cant + 1);
+            setStockk(stockk - 1);
+    }
+
+    const resCant = () => {
+            setCant(cant - 1);
+            setStockk(stockk + 1)
+    }
+
+    return (
+        <div>
+            <p>Item detail</p>
+            <button onClick={resCant}>-</button>
+            <p>{cant}</p>
+            <button onClick={sumCant}>+</button>
+            <button>Agregar a carrito</button>
+        </div>
+    );
+}
+
+export default ItemCount;
--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,6 +1,13 @@
+import ItemCount from "../ItemCount";
+
 const ItemListContainer = () => {
 
-    return <h1>PRODUCTOS</h1>;
+    return (
+            <div>
+                <h1>PRODUCTOS</h1>
+                <ItemCount initial={1} stock={5}/>
+            </div>
+        );
     
 }
 
