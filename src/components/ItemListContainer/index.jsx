79ad3ccc917724076a2232aa8ffaf4baf2edd6f0
Gabi Zinyk <gabi.zinyk@gmail.com>
2022-01-20 10:31:18 -0300

    Fix ItemCount component

--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -5,7 +5,7 @@ const ItemListContainer = () => {
     return (
             <div>
                 <h1>PRODUCTOS</h1>
-                <ItemCount initial={1} stock={5}/>
+                <ItemCount initial={1} stock={3} productName={"T-shirt"}/>
             </div>
         );
     
